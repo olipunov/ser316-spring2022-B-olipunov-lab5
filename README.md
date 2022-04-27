b647a6a29ddab2983e403686243e948b5548a26e
Oleg Lipunov <oleglipunov@me.com>
2022-04-26 18:37:27 -0700

    finished final debuging

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 YouTube:
 GitHub: https://github.com/olipunov/ser316-spring2022-B-olipunov-lab5.git
 
-I chose Option B, where code uses Design Patterns being Intertwined 
+CheckStyle: 0 issues
+SpotBugs: 0 issues
+Jacoco: Over 90% (70% is minimum requirement for this assignment) coverage excluding main
 
-Place holder for lab 5
+I chose Option B, where code uses Design Patterns being Intertwined 
 
 Short Summary of Design Patterns.
 
@@ -25,11 +27,48 @@ Mediator:
 2). Responsible for randomly generating enemy on every floor.
 3). Responsible for keeping track of lvl to present with enemy, mid enemy, or boss.
 4). Responsible for generating a small change of dropping a golden chest.
+5). Battle is a tick base.
 
-I will implement Facade design pattern to help with interface to a complex program. This will be a Section between the user and the complex algorithmic code.
+I  implemented Decorator design pattern to help with adding on class benefits to the player and then adding type banefits to player as well. Enemies use the same structure when they are generated.
 
-Then, I will impement Factory or an Abstract Factory to create objects that are similar in nature and that need a multipy of in the program. Like enemy units.
+Then, I  implemented Singleton to represent Battle , SHop , and Money. Each of those classes are required to have a single instance. This was a perfect solution for my needs. 
+It's perfect for this case, we only need a single shop, shingle battle, and single money class for our RPG game.
 
-Lastly, I will implement Singleton pattern to implement the player who will go through the game. Its perfect for this case, we only need a single player for our RPG game.
+Lastly, I will implement Mediator pattern to implement the interactions between two or more classes. Such as when player and enmy fight, or when player and shop instance interact, it also is responsible for 
+logic in those instances, such as what happens when lvl this lvl and player join in Battle. 
 
-There are many more interesting design patters that look very appealing. I might also use decorator for enemies, choosing their classes/attacks. 
+
+Requerement Fulfield.
+Completed Requirements:
+You must be able to choose a class for your character (or have it assigned randomly),
+each class has a different subset of skills or magic. A class would constitute something
+along the lines of ’fighter’, ’mage’, ’thief’, get creative with it!
+You should be able to choose a type for your character (or have it assigned randomly),
+each race should have one passive skill that gets better after leveling up or that gives
+them advantage over another. rate A type would constitute something like, ’orc’,
+’elf’, ’dwarf’, ’human’. Perhaps orcs are stronger naturally, perhaps elves are the
+most nimble and can dodge better, maybe humans have less HP but gain exp at a
+faster, get creative with it!
+You should encounter a randomly generated ’small’ enemy every floor, a ’medium’
+enemy every 5 floors, and a ’BOSS’ every 10 floors.
+All enemies should get harder for each floor you travel to, for example, an enemy on
+floor 2 should be much, much weaker than an enemy on floor 48!
+After each battle you should have a small chance to find a chest that
+Your character should have at minimum:
+– Stats: Attack, Health, Mana/Other Resource such as cooldown, Defense, and
+speed. Others can be added.
+– Should have at least 1 skill or magic power they can use
+– Physical attacks should have a chance to critically strike (double damage)
+– All attacks should have a chance to fail/miss
+– Attacks should deal a min damage of 1
+Attacks and item usage should happen in a turn based fashion, the participant
+with the highest speed should go first.
+If you lose to a regular enemy you go back to the first floor.
+If you lose to a boss you lose the game.
+Enemies get harder each lvl.
+The combat in the game is turn base.
+The end goal of the game is to beat the final boss (lvl 100).
+The shop should sell 3 random objects.
+After each floor win you go up to the next floor
+You gain experience from wining battles
+Fighting is a tick base.
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -6,8 +6,7 @@
 
 package test.java;
 
-
-import com.sun.source.tree.NewArrayTree;
+import jdk.swing.interop.SwingInterOpUtils;
 import main.java.Battle;
 import main.java.Money;
 import main.java.Shop;
@@ -23,7 +22,6 @@ import main.java.baseunits.Player;
 import main.java.baseunits.PlayerType;
 import org.junit.Assert;
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 /**
@@ -31,7 +29,7 @@ import org.junit.rules.ExpectedException;
  * @author : Oleg Lipunov
  * @version : 4/24/2022, Sunday
  **/
-public class test {
+public class Test {
 
     // For Player class
     private Player current;
@@ -54,7 +52,7 @@ public class test {
     private Shop shop;
 
     /**
-     * Condition before the test.
+     * Condition before the Test.
      * @throws Exception Throw general exception
      */
     @org.junit.Before
@@ -64,26 +62,27 @@ public class test {
     }
 
     /**
-     * Tear down method to dispose of player object after each test
+     * Tear down method to dispose of player object after each Test.
      * @author oleg
      * @throws java.lang.Exception  Throws general exception
      */
     @org.junit.After
-    public void afterMethod() throws Exception{
+    public void afterMethod() throws Exception {
         current = null;
         playerType = null;
-        playerType2= null;
+        playerType2 = null;
 
     }
 
     @Rule
-    public ExpectedException thrown= ExpectedException.none();
+    public ExpectedException thrown = ExpectedException.none();
 
     /**
-     * TEST CASE 1:  Checking player classes class making sure they are created properly and correct.
+     * TEST CASE 1:  Checking player classes class making sure they are
+     * created properly and correct.
      * @author oleg lipunov
      */
-    @Test
+    @org.junit.Test
     public void testPlayer() {
         Assert.assertEquals("Oleg", current.getName());
         Assert.assertEquals("", current.ability());
@@ -100,17 +99,14 @@ public class test {
         Assert.assertEquals("Oleg", current.getName());
         Assert.assertEquals("", current.ability());
         Assert.assertEquals("", current.ability2());
-        System.out.println(current.getName());
-        System.out.println(current.bio());
-        System.out.println(current.getSkill());
-        System.out.println(current.getSkill2());
+
 
         Assert.assertTrue(current.getSkill().equals("Life Drain"));
         Assert.assertTrue(!current.bio().equals("Oleg is a null Warlock"));
-        try{
+        try {
             Assert.assertTrue(current.getSkill2().equals(null));
         } catch (NullPointerException e) {
-
+            System.out.println("Null pointer");
         }
         Assert.assertEquals("", current.ability());
         Assert.assertEquals("", current.ability2());
@@ -121,31 +117,23 @@ public class test {
      * TEST CASE 2:  Checking player types class making sure they are created properly and correct.
      * @author oleg lipunov
      */
-    @Test
+    @org.junit.Test
     public void testTypes() {
 
         current = new Paladin("Oleg");
         playerType = new Elf(current);
-        System.out.println(playerType.ability());
-        System.out.println(playerType.ability2());
         Assert.assertFalse(!current.getName().equals("Oleg"));
         Assert.assertEquals("Healing Hand", playerType.ability());
         Assert.assertEquals("Enhanced Agility", playerType.ability2());
         playerType = new Human(current);
-        System.out.println(playerType.ability());
-        System.out.println(playerType.ability2());
         Assert.assertTrue(current.getName().equals("Oleg"));
         Assert.assertEquals("Healing Hand", playerType.ability());
         Assert.assertEquals("Enhanced Strength", playerType.ability2());
         playerType = new Undead(current);
-        System.out.println(playerType.ability());
-        System.out.println(playerType.ability2());
         Assert.assertFalse(!current.getName().equals("Oleg"));
         Assert.assertEquals("Healing Hand", playerType.ability());
         Assert.assertEquals("Enhanced Intelligence", playerType.ability2());
         playerType = new Troll(current);
-        System.out.println(playerType.ability());
-        System.out.println(playerType.ability2());
         Assert.assertFalse(!current.getName().equals("Oleg"));
         Assert.assertEquals("Healing Hand", playerType.ability());
         Assert.assertEquals("Enhanced Intuition", playerType.ability2());
@@ -155,38 +143,25 @@ public class test {
      * TEST CASE 3:  Checking player base classes making sure they are created properly and correct.
      * @author oleg lipunov
      */
-    @Test
+    @org.junit.Test
     public void testBaseUintPlayer() {
 
         current = new Paladin("Oleg");
-        System.out.println(current.getAgility());
         Assert.assertEquals(3, current.getAgility());
-        System.out.println(current.getStrength());
         Assert.assertEquals(7, current.getStrength());
-        System.out.println(current.getIntuition());
         Assert.assertEquals(5, current.getIntuition());
-        System.out.println(current.getIntelligence());
         Assert.assertEquals(5, current.getIntelligence());
-        System.out.println(current.getVitality());
         Assert.assertEquals(5, current.getVitality());
-        System.out.println(current.getTypeBonus());
         Assert.assertNull(current.getTypeBonus());
-        System.out.println(current.getType());
         Assert.assertNull(current.getType());
-        System.out.println(current.getClassType());
         Assert.assertEquals("Paladin", current.getClassType());
         current.setTypeBonus("bazooka");
-        System.out.println(current.getTypeBonus());
         Assert.assertEquals("bazooka", current.getTypeBonus());
         current.setClassType("smerch");
-        System.out.println(current.getClassType());
         Assert.assertEquals("smerch", current.getClassType());
-        System.out.println(current.toString());
         Assert.assertNotNull(current.toString());
         current.setName("Vasia");
-        System.out.println(current.getName());
         Assert.assertEquals("Vasia", current.getName());
-        System.out.println(current.getAbility());
         Assert.assertNotNull(current.getAbility());
     }
 
@@ -194,42 +169,27 @@ public class test {
      * TEST CASE 3:  Checking player base classes making sure they are created properly and correct.
      * @author oleg lipunov
      */
-    @Test
+    @org.junit.Test
     public void testBaseUintPlayerType() {
 
         current = new Paladin("Oleg");
         playerType = new Troll(current);
-        System.out.println(playerType.getAgility());
         Assert.assertEquals(3, playerType.getAgility());
-        System.out.println(playerType.getStrength());
         Assert.assertEquals(7, playerType.getStrength());
-        System.out.println(playerType.getIntuition());
         Assert.assertEquals(6, playerType.getIntuition());
-        System.out.println(playerType.getIntelligence());
         Assert.assertEquals(5, playerType.getIntelligence());
-        System.out.println(playerType.getVitality());
         Assert.assertEquals(5, playerType.getVitality());
-        System.out.println(playerType.getTypeBonus());
         Assert.assertNull(playerType.getTypeBonus());
-        System.out.println(playerType.getType());
         Assert.assertNull(playerType.getType());
-        System.out.println(playerType.getClassType());
         Assert.assertEquals(null, playerType.getClassType());
         playerType.setTypeBonus("bazooka");
-        System.out.println(playerType.getTypeBonus());
         Assert.assertEquals("bazooka", playerType.getTypeBonus());
         playerType.setClassType("smerch");
-        System.out.println(playerType.getClassType());
         Assert.assertEquals("smerch", playerType.getClassType());
-        System.out.println(playerType.toString());
         Assert.assertNotNull(playerType.toString());
         Assert.assertNotNull(playerType.bio());
-        System.out.println(playerType.getSkill());
-        System.out.println("TestTT");
-        System.out.println(playerType.getName());
         Assert.assertEquals(null, playerType.getName());
         playerType.setVitality();
-        System.out.println(playerType.getVitality());
         Assert.assertEquals(5, playerType.getVitality());
 
     }
@@ -237,7 +197,7 @@ public class test {
     /**
      * Test case for money and its methods.
      */
-    @Test
+    @org.junit.Test
     public void  testMoney() {
         money = new Money();
         Money.setMoney(100);
@@ -251,7 +211,7 @@ public class test {
     /**
      * Test case for battle singleton class.
      */
-    @Test
+    @org.junit.Test
     public void testBattle()  {
         Assert.assertNotNull(Battle.getInstance(playerType2, 10));
         Assert.assertNotNull(Battle.getInstance(playerType2, 1));
@@ -269,7 +229,7 @@ public class test {
      * Test case for Shop singleton class.
      * We run thi stest a few times since there are many different random outcomes.
      */
-    @Test
+    @org.junit.Test
     public void testShop()  {
         Assert.assertNotNull(Shop.getShop(playerType2));
         Assert.assertNotNull(Shop.getShop(playerType2));
